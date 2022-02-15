dd6869e274a47dc9a1f19fb71e96e1c28fafd852
zo-lew <zoe.lewis@students.iaac.net>
2022-02-15 23:49:36 +0000

    aesthetic changes

--- example2.1 color/script.js
+++ example2.1 color/script.js
@@ -171,12 +171,12 @@ function init() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color(1, 1, 1);
   camera = new THREE.PerspectiveCamera(
-    75,
+    45,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
   );
-  camera.position.z = -30;
+  camera.position.y = -130;
 
   // create the renderer and add it to the html
   renderer = new THREE.WebGLRenderer({ antialias: true });
